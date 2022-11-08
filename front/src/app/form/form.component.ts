4c116d22da17c35bcc61640e110c344da5734698
ridvanaktepe <ridvan_aktepe_07@hotmail.com>
2022-11-09 00:21:41 +0300

    create patch

--- front/src/app/form/form.component.ts
+++ front/src/app/form/form.component.ts
@@ -3,41 +3,50 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from '../model/User';
 import { IUser } from '../model/IUser';
 import { DbServiceService } from '../service/db-service.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
+  providers: [DbServiceService],
 })
 export class FormComponent implements OnInit {
-
   isChecked: boolean = false;
 
-  user: User = { UserId: "", UserName: "", UserSurname: "", UserEmail: "", UserPassword: "", };
+  user: User = {
+    UserId: '',
+    UserName: '',
+    UserSurname: '',
+    UserEmail: '',
+    UserPassword: '',
+  };
   userList: User[] = [];
   iuserList?: IUser;
-
+ 
   constructor(private service: DbServiceService) {
     // service.GetUserList().subscribe(response => { this.userList = response })
   }
 
-  // constructor(){}
-
   UserForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    surname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    surname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(2)])
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
   });
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   PostForm() {
     console.log(this.UserForm.value);
     console.log(this.UserForm.status);
-    console.log("-----------------------------" + this.user.UserId);
+    console.log('-----------------------------' + this.user.UserId);
 
     if (this.UserForm.valid) {
       this.user.UserName = this.UserForm.value.name!;
@@ -45,9 +54,14 @@ export class FormComponent implements OnInit {
       this.user.UserEmail = this.UserForm.value.email!;
       this.user.UserPassword = this.UserForm.value.password!;
       console.log(this.user);
-      this.service.AddUser(this.user).subscribe({ next: (response) => { console.log(response) }, error: (error) => { console.log(error) } });
-
+      this.service.AddUser(this.user).subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
     }
   }
-
 }
--- front/src/app/service/db-service.service.ts
+++ front/src/app/service/db-service.service.ts
@@ -1,23 +1,45 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable, map, tap, delay, take, catchError, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  Observable,
+  map,
+  tap,
+  delay,
+  take,
+  catchError,
+  throwError,
+} from 'rxjs';
 import { User } from '../model/User';
 
 @Injectable()
 export class DbServiceService {
+  _url = 'http://localhost:4201/api/user/';
 
-  _url = "http://localhost:4201/api/user/";
-
-  constructor(private http: HttpClient) { }
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+  constructor(private http: HttpClient) {}
 
   // getall
   GetUserList(): Observable<User[]> {
-    return this.http.get<User[]>(this._url + "getall").pipe(tap(data => { console.log(data); }), catchError(this.handleError));
+    return this.http.get<User[]>(this._url + 'getall').pipe(
+      tap((data) => {
+        console.log(data);
+      }),
+      catchError(this.handleError)
+    );
   }
 
   // create
   AddUser(_user: User): Observable<User> {
-    return this.http.post<User>(this._url + "create", JSON.stringify(_user));
+    return this.http.post<User>(
+      this._url + 'create',
+      JSON.stringify(_user),
+      this.httpOptions
+    );
   }
 
   private handleError(error: any) {
